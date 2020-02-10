42b10364c450648bb4a2c0c9954b0ae0f8ae62ac
liuyuanbwin <liuyuanbwin@163.com>
2020-02-10 22:42:55 +0800

    1.完成第一页布局

--- day07/02.vue-cms/src/main.js
+++ day07/02.vue-cms/src/main.js
@@ -3,18 +3,39 @@ console.log('ok')
 
 import Vue from 'vue'
 
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import VueResource from 'vue-resource'
+Vue.use(VueResource)
+
+
+
 // 导入 MUI 的样式
 import './lib/mui/css/mui.min.css'
 
+import './lib/mui/css/icons-extra.css';
+
+import './lib/mui/fonts/mui-icons-extra.ttf'
+
 //按需导入 Mint-UI 中的组件
 import {
-  Header
+  Header,
+  Swipe,
+  SwipeItem
 } from 'mint-ui'
+
 import app from './App.vue'
 Vue.component(Header.name, Header)
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
+
+import router from './router.js'
 
 
 var vm = new Vue({
   el: '#app',
-  render: c => c(app)
+  render: c => c(app),
+  router
 })
--- day07/02.vue-cms/src/router.js
+++ day07/02.vue-cms/src/router.js
@@ -1,12 +1,34 @@
 import VueRouter from 'vue-router'
 
-
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import ShopcarContainer from './components/tabbar/ShopcartContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
 
 // 3. 创建路由对象
 var router = new VueRouter({
-  routes: [
-
-  ]
+  routes: [{
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      component: HomeContainer
+    },
+    {
+      path: '/member',
+      component: MemberContainer
+    },
+    {
+      path: '/shopcar',
+      component: ShopcarContainer
+    },
+    {
+      path: '/search',
+      component: SearchContainer
+    }
+  ],
+  linkActiveClass: 'mui-active' //覆盖默认的路由高亮度类,默认的类是 router-link-activate
 })
 
 // 把路由对象暴露出去
